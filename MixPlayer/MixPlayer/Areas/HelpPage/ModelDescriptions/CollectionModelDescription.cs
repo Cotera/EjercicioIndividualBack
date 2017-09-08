cbd629e50a7622d940b3710cc24af0453f9dec19
rcotera <rcotera@email>
2017-09-08 16:02:56 +0200

    Commit inicial con los paquetes instalados.

---
+++ MixPlayer/MixPlayer/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace MixPlayer.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ MixPlayer/MixPlayer/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MixPlayer.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
